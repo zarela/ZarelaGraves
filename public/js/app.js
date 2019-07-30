e3f6c594f9bddc3c7de7c56d431b2c1d47079b18
Zarela Graves <zarelavp@yahoo.com>
2019-07-30 00:33:50 -0400

    Adds resume to navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,52 @@
-(function(){
-  angular
-  .module('zareApp', ['ui.router'])
-  .config(MainRouter);
+(function() {
+  angular.module("zareApp", ["ui.router"]).config(MainRouter);
 
-  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+  MainRouter.$inject = [
+    "$stateProvider",
+    "$urlRouterProvider",
+    "$locationProvider"
+  ];
 
-  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider){
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "home.html"
+      })
+      .state("about", {
+        url: "/about",
+        templateUrl: "about.html"
+      })
+      .state("work", {
+        url: "/work",
+        templateUrl: "work.html"
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "contact.html"
+      })
+      .state("job-trail", {
+        url: "/projects/job-trail",
+        templateUrl: "projects/job-trail.html"
+      })
+      .state("black-jack", {
+        url: "/projects/black-jack",
+        templateUrl: "projects/black-jack.html"
+      })
+      .state("shopmate", {
+        url: "/projects/shopmate",
+        templateUrl: "projects/shopmate.html"
+      })
+      .state("goal-digger", {
+        url: "/projects/goal-digger",
+        templateUrl: "projects/goal-digger.html"
+      });
 
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: "home.html",
-  })
-  .state('about', {
-    url: '/about',
-    templateUrl: "about.html"
-  })
-  .state('work', {
-    url: '/work',
-    templateUrl: "work.html"
-  })
-  .state('contact', {
-    url: '/contact',
-    templateUrl: "contact.html"
-  })
-  .state('job-trail', {
-    url: '/projects/job-trail',
-    templateUrl: "projects/job-trail.html"
-  })
-  .state('black-jack', {
-    url: '/projects/black-jack',
-    templateUrl: "projects/black-jack.html"
-  })
-  .state('shopmate', {
-    url: '/projects/shopmate',
-    templateUrl: "projects/shopmate.html"
-  })
-  .state('goal-digger', {
-    url: '/projects/goal-digger',
-    templateUrl: "projects/goal-digger.html"
-  })
+    $urlRouterProvider.otherwise("/");
 
-  $urlRouterProvider.otherwise('/')
-
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  }); //locationProvider
-
-  }; //end of routes
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }
 })();
